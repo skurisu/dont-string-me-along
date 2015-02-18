290e90519cd8065f1ced1fb8c77d60b6c0bd5798
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-02-17 20:37:25 -1000

    adds palindrome function

--- dontstringmealong.js
+++ dontstringmealong.js
@@ -1,7 +1,7 @@
 (function () {
-  var str = "9899100101103104105";
+  var num = "9899100101103104105";
 
-  function missingNumPush (){
+  function missingNumPush (str){
   var missingNums = [];
     for (var i = 0; i < 10; i++){
       if (str.indexOf(i) === -1){
@@ -10,23 +10,48 @@
     }
   return missingNums;
   }
-  missingNumPush();
+  missingNumPush(num);
 })();
 
 (function () {
   // console.log("test");
-  var sample = "Jay Won";
-  var whiteSpace = sample.indexOf(" ");
+  var name = "Jay Won";
+  var space = sample.indexOf(" ");
 
-  function swapWords (){
-    var secondWord = sample.slice(0,whiteSpace);
-    var firstWord = sample.slice(whiteSpace, sample.length);
+  function swapWords (sample,whitespace){
+    var secondWord = sample.slice(0,whitespace);
+    var firstWord = sample.slice(whitespace, sample.length);
     var swapped = (firstWord + ", " + secondWord);
     return swapped;
     //return(firstWord + ", " + secondWord);
   }
 
-  swapWords();
+  swapWords(name,space);
 })();
 
+(function () {
 
+  function circVar (str1,str2) {
+    // concat str2 with str2
+    // check for str1 within concat str2 (check length of str1)
+  }
+})();
+
+(function () {
+
+  var myPalindrome = "racecar";
+
+  function palindrome (str1) {
+
+    var reverseString = str1.split("").reverse().join("");
+
+    if(str1 === reverseString) {
+      console.log("true");
+    }
+    else {
+      console.log("false!");
+    }
+  }
+
+  palindrome(myPalindrome);
+})();
